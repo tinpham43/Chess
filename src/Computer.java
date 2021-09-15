f0664c48cc2058c5ec7f2afc2074b660310b4395
tinpham43 <tpham34@asu.edu>
2021-09-15 15:28:10 -0700

    computer progress

--- src/Computer.java
+++ src/Computer.java
@@ -2,9 +2,22 @@ import java.awt.Color;
 
 public class Computer {
 
-	public static Piece valueIteration(int lookup, int turnCount)
+	private static final int BOARD_SIZE = 8;
+	private List<Pair<Piece, Piece>> moveSet;
+	private Piece liftedPiece;
+	private Piece newPiece;
+	
+	public Computer()
 	{
-		if(turnCount <= 1)
+		moveSet = new ArrayList<Pair<Piece, Piece>>();
+		Pair<Piece, Piece> bothPieces = valueIteration(20, 1);
+		liftedPiece = bothPieces.getKey();
+		newPiece = bothPieces.getValue();
+	}
+	
+	public static Pair<Piece, Piece> valueIteration(int lookup, int turnCount)
+	{
+		/*if(turnCount <= 1)
 			Board.getInstance().movePiece(
 			Board.getInstance().getPiece(1, 4), 
 			Board.getInstance().getPiece(2, 4),
@@ -12,8 +25,43 @@ public class Computer {
 		for(int i = 0; i < lookup; i++)
 		{
 			;
+		}*/
+		
+		for(int i = 0; i < BOARD_SIZE; i++)
+		{
+			for(int j = 0; j < BOARD_SIZE; j++)
+			{
+				if(board.get(i).get(j).getForeground().equals(Color.black) &&
+				  !board.get(i).get(j).getActionCommand().equals(""))
+				{
+					for(int k = 0; k < BOARD_SIZE; k++)
+					{
+						for(int l = 0; l < BOARD_SIZE; l++)
+						{
+							if(MovePiece.validMove(board.get(i).get(j), board.get(k).get(l)))
+							{
+								moveSet.add(Pair(board.get(i).get(j), board.get(k).get(l)));
+							}
+						}
+					}
+				}
+			}
 		}
 		
+		int rand = (int)Math.floor(Math.random()*(moveSet.size() + 1))
+		
+		
+		
 		return new Piece();
+	}
+	
+	public Piece getLifted()
+	{
+		return liftedPiece;
+	}
+	
+	public Piece getNew()
+	{
+		return newPiece;
 	}
 }
